cd280d2fabd87ff22a688bbe01a6add11704879c
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-01-25 16:32:58 -0700

    Add stuff

---
+++ src/main/java/frc/robot/BaseRobot/RobotBase.java
@@ -0,0 +1,57 @@
+package frc.robot.BaseRobot;
+
+//Controllers
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+
+//Pneumatic System
+/*
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+*/
+
+//Motor Control
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+//Sensors
+//import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+//Base Robot
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
+
+//Other
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class RobotBase {
+    // Controllers
+    public static XboxController xbox = new XboxController(0);
+    public static Joystick main = new Joystick(1);
+
+    // Pneumatic System
+
+    // Motor Control
+    public static CANSparkMax rightDrive = new CANSparkMax(1, MotorType.kBrushless);
+    public static CANSparkMax leftDrive = new CANSparkMax(2, MotorType.kBrushless);
+
+    // Sensors
+    public static RelativeEncoder rightEncoder = rightDrive.getEncoder();
+    public static RelativeEncoder leftEncoder = leftDrive.getEncoder();
+    public static ADXRS450_Gyro Gyro = new ADXRS450_Gyro();
+
+    // Base robot
+    public static PowerDistribution PDP = new PowerDistribution();
+    public static PneumaticsControlModule PCM = new PneumaticsControlModule();
+
+
+    // Shuffleboard
+    public static void shuffleboard() {
+        SmartDashboard.putNumber("Right Encoder", rightEncoder.getPosition());
+        SmartDashboard.putNumber("Left Encoder", leftEncoder.getPosition());
+    }
+}
---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,36 @@
+package frc.robot;
+
+//Import the central system for the components
+import frc.robot.BaseRobot.*;
+
+/*
+ * Button numbers: [1: trigger] [2: sidebutton] [3:labeled] [4: labeled] [5:
+ * labeled] [6: labeled] [7: labeled] [8: labeled] [9: labeled] [10: labeled]
+ * [11: labeled] [12: labeled]
+ */
+
+public class Drivetrain {
+
+    // Declare variables for the speed modifiers
+    private static double speedModifierDriving;
+
+    // Main drive method
+    public static void drive() {
+
+        // Set the speed modifier depending on wether or not the trigger(1) on the
+        // joystick is held down
+        if (RobotBase.main.getRawButton(1)) {
+            speedModifierDriving = 1;
+        } else {
+            speedModifierDriving = 0.5;
+        }
+
+        // Set variables for the left and right motors to the controllers axis, using
+        // both the up/down and left/right values and some math; multiplied by the speed
+        // modifier
+        RobotBase.leftDrive.set(
+                (-RobotBase.main.getRawAxis(1) * 0.5 - RobotBase.main.getRawAxis(0) * 0.5) * speedModifierDriving);
+        RobotBase.rightDrive.set(
+                (-RobotBase.main.getRawAxis(1) * 0.5 + RobotBase.main.getRawAxis(0) * 0.5) * speedModifierDriving);
+    }
+}
