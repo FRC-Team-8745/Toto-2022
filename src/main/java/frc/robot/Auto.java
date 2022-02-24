fe68b6fe0c31aa4d7b444f0fc659033291432161
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-02-24 10:40:52 -0700

    Update Auto.java

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -20,14 +20,16 @@ public class Auto {
                 step += this.auto.driveFeet(-4, 0.5, true);
                 break;
             case 1:
-                step += this.auto.autoShoot(5, 1, true);
+                //TODO: find where these come from and add them again
+                //step += this.auto.autoShoot(5, 1, true);
                 break;
             case 2:
                 step += this.auto.turnDegrees(45, 1, true);
                 break;
             case 3:
                 step += this.auto.driveFeet(4, 1, true);
-                this.auto.autoIntake(4, 0.5, true);
+                //TODO: find where these come from and add them again
+                //this.auto.autoIntake(4, 0.5, true);
                 break;
             case 4:
 
