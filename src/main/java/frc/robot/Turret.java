53073cd2c3eaa3ffe5ccc5a24ba6a12333355d97
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-02 19:32:57 -0700

    Add Turret.java

---
+++ src/main/java/frc/robot/Turret.java
@@ -0,0 +1,36 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Turret extends SubsystemBase {
+
+	public BrushlessNEO turret = new BrushlessNEO(7, false);
+
+	private final int kTurretRatio = 120;
+	// Turret error in degrees
+	private final double kTurretError = 5;
+	private final double kTurretProportional = 0.2;
+
+	@Override
+	public void periodic() {
+
+	}
+
+	public Turret() {
+		turret.setRamp(0.5);
+		turret.idleMode(IdleMode.kBrake);
+		turret.resetPosition();
+	}
+
+	public void rotateToDegrees(double targetDegrees, double speed) {
+		double turretErrorRotations = kTurretError / 360;
+		double targetRotations = targetDegrees / 360;
+
+		turret.set(speed - ((targetRotations * kTurretRatio) - turret.getPosition()) * kTurretProportional);
+
+		if (Math.abs(turret.getPosition() * kTurretRatio) < turretErrorRotations)
+			turret.stop();
+	}
+}
