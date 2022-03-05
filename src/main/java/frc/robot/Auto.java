4747a86e460a7c4f75a1bceac3ae5c55ba7289d5
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-05 15:11:59 -0700

    Add resetOnEnd functionality & fix speed

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -16,7 +16,7 @@ public Auto(AutoCommands auto_, Turret turret_){
         new InstantCommand(() -> Robot.climberLeft.set(0.2)),
         new WaitCommand(0.5),
         new InstantCommand(() -> Robot.climberLeft.stop()),
-        new WaitUntilCommand(() -> autoCom.driveFeet(-7, 0.2, true)),
+        new WaitUntilCommand(() -> autoCom.driveFeet(7, -0.2, true)),
         //new WaitCommand(1),
         new WaitUntilCommand(() -> autoCom.turnDegrees(120, 0.2, true)),
         new WaitUntilCommand (() -> turret.rotateDegrees(-120, 0.5)),
--- src/main/java/frc/robot/AutoCommands.java
+++ src/main/java/frc/robot/AutoCommands.java
@@ -8,45 +8,50 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
 public class AutoCommands extends SubsystemBase {
-    PIDController drivePID;
-    PIDController turnPID;
-    double imu;
-    AHRS IMU = new AHRS(Port.kUSB);
-    @Override
-    public void periodic(){
-        imu = IMU.getRawGyroZ();
-    }
-    double circumference;
+	PIDController drivePID;
+	PIDController turnPID;
+	double imu;
+	AHRS IMU = new AHRS(Port.kUSB);
 
-    public AutoCommands(Double[] pidDrive_, Double[] pidTurn_,
-            double circumference_) {
-        drivePID = new PIDController(pidDrive_[0], pidDrive_[1], pidDrive_[2]);
-        turnPID = new PIDController(pidTurn_[0], pidTurn_[1], pidTurn_[2]);
-        imu = 0;
-        circumference = circumference_;
-    }
+	@Override
+	public void periodic() {
+		imu = IMU.getRawGyroZ();
+	}
 
-    public Boolean driveFeet(double feet, double speed, boolean resetOnEnd) {
-        if (Math.abs(((Robot.right.getPosition() * circumference) / 12) - feet) < 0.25) {
-            Robot.drive.stopDrive();
-            return true;
-        }
+	double circumference;
 
-        Robot.left.set(speed);
-        Robot.right.set(speed + drivePID.calculate(Robot.right.getPosition() - Robot.left.getPosition(), 0));
-        return false;
-    }
+	public AutoCommands(Double[] pidDrive_, Double[] pidTurn_,
+			double circumference_) {
+		drivePID = new PIDController(pidDrive_[0], pidDrive_[1], pidDrive_[2]);
+		turnPID = new PIDController(pidTurn_[0], pidTurn_[1], pidTurn_[2]);
+		imu = 0;
+		circumference = circumference_;
+	}
 
-    public Boolean turnDegrees(double degrees, double speed, boolean resetOnEnd) {
-        if (Math.abs(imu - degrees) < 5) {
-            Robot.drive.stopDrive();
-            return true;
-        }
+	public Boolean driveFeet(double feet, double speed, boolean resetOnEnd) {
+		if (Math.abs(((Robot.right.getPosition() * circumference) / 12) - feet) < 0.25) {
+			Robot.drive.stopDrive();
+			if (resetOnEnd)
+				Robot.drive.resetEncoders();
+			return true;
+		}
 
-        Robot.left.set(speed);
-        Robot.right.set(-speed - turnPID.calculate(imu - degrees, 0));
-        return false;
-    }
+		Robot.left.set(speed);
+		Robot.right.set(speed + drivePID.calculate(Robot.right.getPosition() - Robot.left.getPosition(), 0));
+		return false;
+	}
+
+	public Boolean turnDegrees(double degrees, double speed, boolean resetOnEnd) {
+		if (Math.abs(imu - degrees) < 5) {
+			Robot.drive.stopDrive();
+			if (resetOnEnd)
+				Robot.drive.resetEncoders();
+			return true;
+		}
+
+		Robot.left.set(speed);
+		Robot.right.set(-speed - turnPID.calculate(imu - degrees, 0));
+		return false;
+	}
 }
