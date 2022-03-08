8796a9ad050ef938a982bfa0f13fc4d311377149
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-07 17:52:26 -0700

    annd AutoCommands

--- src/main/java/frc/robot/AutoCommands.java
+++ src/main/java/frc/robot/AutoCommands.java
@@ -1,43 +1,53 @@
 package frc.robot;
 
-//import java.util.concurrent.TimeUnit;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class AutoCommands {
-    PIDController drivePID;
-    PIDController turnPID;
-    // TODO: Replace 'imu' with the imu's getDegrees method
-    double imu;
-    double circumference;
+public class AutoCommands extends SubsystemBase {
+	PIDController drivePID;
+	PIDController turnPID;
+	double imu;
+	AHRS IMU = new AHRS(Port.kUSB);
 
-    public AutoCommands(Double[] pidDrive_, Double[] pidTurn_,
-            double circumference_) {
-        drivePID = new PIDController(pidDrive_[0], pidDrive_[1], pidDrive_[2]);
-        turnPID = new PIDController(pidTurn_[0], pidTurn_[1], pidTurn_[2]);
-        imu = 0;
-        circumference = circumference_;
-    }
+	@Override
+	public void periodic() {
+		imu = IMU.getRawGyroZ();
+	}
 
-    public int driveFeet(double feet, double speed, boolean resetOnEnd) {
-        if (Math.abs((Robot.right.getPosition() * circumference) - feet) < 0.25) {
-            Robot.drive.stopDrive();
-            return 1;
-        }
+	final double kDiameter = 6;
 
-        Robot.left.set(speed);
-        Robot.right.set(speed + drivePID.calculate(Robot.right.getPosition() - Robot.left.getPosition(), 0));
-        return 0;
-    }
+	public AutoCommands(Double[] pidDrive_, Double[] pidTurn_) {
+		drivePID = new PIDController(pidDrive_[0], pidDrive_[1], pidDrive_[2]);
+		turnPID = new PIDController(pidTurn_[0], pidTurn_[1], pidTurn_[2]);
+		imu = 0;
+	}
 
-    public int turnDegrees(double degrees, double speed, boolean resetOnEnd) {
-        if (Math.abs(imu - degrees) < 5) {
-            Robot.drive.stopDrive();
-            return 1;
-        }
+	public Boolean driveFeet(double feet, double speed, boolean resetOnEnd) {
+		if (Math.abs(feet / ((kDiameter / 12) * Math.PI)) < Math.abs(-Robot.right.getPosition() / 10.71)) {
+			Robot.drive.stopDrive();
+			if (resetOnEnd)
+				Robot.drive.resetEncoders();
+			return true;
+		}
 
-        Robot.left.set(speed);
-        Robot.right.set(-speed - turnPID.calculate(imu - degrees, 0));
-        return 0;
-    }
+		Robot.left.set(-speed);
+		Robot.right.set(-speed + drivePID.calculate(Robot.right.getPosition() - Robot.left.getPosition(), 0));
+		return false;
+	}
+
+	public Boolean turnDegrees(double degrees, double speed, boolean resetOnEnd) {
+		if (Math.abs(imu - degrees) < 5) {
+			Robot.drive.stopDrive();
+			if (resetOnEnd)
+				Robot.drive.resetEncoders();
+			return true;
+		}
+
+		Robot.left.set(speed);
+		Robot.right.set(-speed - turnPID.calculate(imu - degrees, 0));
+		return false;
+	}
 }
