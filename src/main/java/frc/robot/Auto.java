f96c3f4a1ce212d89022f767cf2c4a7ea8e6d7df
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-08 20:56:52 -0700

    Reverse climber direction for new intake deployment

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj2.command.*;
 public class Auto {
 	public static AutoCommands auto = new AutoCommands();
 
+	// Old code, in case something doesn't work
+
+	/*/
 	public void fullAuto() {
 		new SequentialCommandGroup(
 				new InstantCommand(() -> Robot.climberLeft.set(0.5)),
@@ -17,6 +20,28 @@ public class Auto {
 				new WaitUntilCommand(() -> auto.driveFeet(7.583, -0.25, true)),
 				new InstantCommand(() -> Robot.intake.stop()),
 				new InstantCommand(() -> Robot.autoShooter.shootFull.schedule())
+		).schedule();
+	}
+	/*/
+
+	public void fullAuto() {
+		new SequentialCommandGroup(
+				new InstantCommand(() -> deployIntake()),
+				new InstantCommand(() -> Robot.autoShooter.shootFull.schedule()),
+				new WaitCommand(4),
+				new InstantCommand(() -> Robot.intake.set(1)),
+				new WaitUntilCommand(() -> auto.driveFeet(7.583, 0.25, true)),
+				new WaitUntilCommand(() -> auto.driveFeet(7.583, -0.25, true)),
+				new InstantCommand(() -> Robot.intake.stop()),
+				new InstantCommand(() -> Robot.autoShooter.shootFull.schedule())
+		).schedule();
+	}
+
+	public void deployIntake() {
+		new SequentialCommandGroup(
+			new InstantCommand(() -> Robot.climberLeft.set(-0.5)),
+			new WaitCommand(0.3),
+			new InstantCommand(() -> Robot.climberLeft.stop())
 		).schedule();
 	}
 }
