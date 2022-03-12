a77a5263e849ec339216215901f19b5aa10db9ec
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-12 08:11:31 -0700

    add basic odometry functions

---
+++ src/main/java/frc/robot/Odometry.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+
+public class Odometry {
+
+	public static final double kRobotStartPosX = 0;
+	public static final double kRobotStartPosY = 0;
+	public static AHRS IMU = new AHRS(Port.kUSB1);
+	public DifferentialDriveOdometry robotOdometry = new DifferentialDriveOdometry(
+			new Rotation2d(degreesToRadians(IMU.getYaw())),
+			new Pose2d(kRobotStartPosX, kRobotStartPosY, new Rotation2d()));
+
+	public double degreesToRadians(double degrees) {
+		return degrees * (Math.PI / 180);
+	}
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,16 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax.IdleMode;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
