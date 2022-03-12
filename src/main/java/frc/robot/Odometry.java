a9e297e781c77b176f12bc3559423c3c957f65e5
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-12 12:07:30 -0700

    seperate declarations

--- src/main/java/frc/robot/Odometry.java
+++ src/main/java/frc/robot/Odometry.java
@@ -6,12 +6,22 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Odometry {
-
+public class Odometry extends SubsystemBase {
 	public static final double kRobotStartPosX = 0;
 	public static final double kRobotStartPosY = 0;
 	public static AHRS IMU = new AHRS(Port.kUSB1);
+
+	public Odometry() {
+		Rotation2d robotRotation = new Rotation2d(degreesToRadians(IMU.getYaw()));
+		Pose2d robotPosition = new Pose2d(kRobotStartPosX, kRobotStartPosY, new Rotation2d());
+	}
+
+	@Override
+	public void periodic() {
+	}
+
 	public DifferentialDriveOdometry robotOdometry = new DifferentialDriveOdometry(
 			new Rotation2d(degreesToRadians(IMU.getYaw())),
 			new Pose2d(kRobotStartPosX, kRobotStartPosY, new Rotation2d()));
