8e997faa96860e2b5ba4e3d6f7fc492a511bd4d3
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-17 14:16:59 -0700

    x

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import javax.management.InstanceAlreadyExistsException;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 
@@ -55,7 +57,8 @@ public class Autonomous {
 						new InstantCommand(() -> Robot.intake.set(0.5)),
 						new WaitUntilCommand(() -> commands.driveFeet(4.25, 1, true)),
 						new WaitUntilCommand(() -> commands.driveFeet(6.25, -1, true)),
-						new InstantCommand(() -> Robot.shootCommands.shootDouble())
+						new InstantCommand(() -> Robot.shootCommands.shootDouble()),
+						new WaitCommand(4)
 
 				).schedule();
 				break;
