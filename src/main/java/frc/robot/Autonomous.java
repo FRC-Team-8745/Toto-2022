4fbea3ba430ca5acc126247b42188cfe4f69764c
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-03-18 10:46:51 -0700

    Remove unneeded Import

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import javax.management.InstanceAlreadyExistsException;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 
